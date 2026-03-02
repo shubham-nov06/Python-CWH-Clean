983c9bfe8e2dad1b3e52f28a94b436a679ba2346
shubham-nov06 <dograshubham06@gmail.com>
2026-03-02 12:32:05 +0530

    Added file 03conditional.py

---
+++ CHAPTER-6/03conditional.py
@@ -0,0 +1,18 @@
+from Demos.security.security_enums import Enum
+
+complexnum = complex(2, 3)
+
+print(complexnum)
+
+print(type(complexnum))
+
+PI = 12345
+
+print(PI) 
+
+condition = True
+if condition == True:
+    
+    print("The condition")
+    print("Was true ")
+
