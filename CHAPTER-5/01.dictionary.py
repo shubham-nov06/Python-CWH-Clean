717efda72cae7bf31ed62f2107fdf6cdc76cfda5
shubham-nov06 <dograshubham06@gmail.com>
2026-03-02 12:32:01 +0530

    Added file 01.dictionary.py

---
+++ CHAPTER-5/01.dictionary.py
@@ -0,0 +1,13 @@
+marks = {  # DICTIONARY: Stores key-value pairs
+    "shubham": 98,
+    "Pankaj": 98,
+}
+print(marks, type(marks))  # Prints the dictionary and its type
+
+marks2 = (1, 2, 3, "Shubham")  # TUPLE: Immutable sequence of values
+print(marks2, type(marks2))  # Prints the tuple and its type
+
+marks3 = [1, 23, 4, "Shubham", "Pankaj"]  # LIST: Ordered, mutable collection
+print(marks3, type(marks3))  # Prints the list and its type
+
+print(marks["shubham"])  # Accesses the value for key 'shubham' in the dictionary
