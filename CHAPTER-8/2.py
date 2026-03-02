4967c202f1c3de86ce2390b7c483438b0ea385ca
shubham-nov06 <dograshubham06@gmail.com>
2026-03-02 09:00:55 +0000

    Updated changes

--- CHAPTER-8/2.py
+++ CHAPTER-8/2.py
@@ -1,7 +1,7 @@
-def calclutaor () :
+def calclutaor():
     num1 = int(input("Enter your number = "))
     num2 = int(input("Enter your number = "))
 
     operator = input("Enter your operation = ")
-    if operator == '*':
-        print("Multiplication is = " , num1 * num2 )
+    if operator == "*":
+        print("Multiplication is = ", num1 * num2)
