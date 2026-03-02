f1ddef778ea40482cabeca02d954b8212d4cee94
shubham-nov06 <dograshubham06@gmail.com>
2026-03-02 09:05:02 +0000

    Updated changes

--- CHAPTER-8/2.py
+++ CHAPTER-8/2.py
@@ -1,10 +1,13 @@
 def calclutaor():
-    num1 = int(input("Enter your number = "))
-    num2 = int(input("Enter your number = "))
+    for i in range(2):
+        num1 = int(input("Enter your number = "))
+        num2 = int(input("Enter your number = "))
 
-    operator = input("Enter your operation = ")
-    if operator == "*":
-        print("Multiplication is = ", num1 * num2)
-    elif operator == "%":
-        print("Remainder is = ", num1 % num2)
+        operator = input("Enter your operation = ")
+        if operator == "*":
+            print("Multiplication is = ", num1 * num2)
+        elif operator == "%":
+            print("Remainder is = ", num1 % num2)
+
+
 calclutaor()
