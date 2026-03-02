b4a4c357ce70cb219566edf044f69f80b33d1b74
shubham-nov06 <dograshubham06@gmail.com>
2026-03-02 12:31:58 +0530

    Added file Q4.py

---
+++ CHAPTER-3/Q4.py
@@ -0,0 +1,5 @@
+name = "Shubham  Is  a good boy " 
+# print(name.find(" ")) 
+print(name.replace("  " , " "))
+print(name)  # strings are immutable which  means that yu cannot change them by running functions on them 
+
