ed8c1b6a251cfefb7f1b9f212a1c50ec6f615203
shubham-nov06 <dograshubham06@gmail.com>
2026-03-02 12:32:02 +0530

    Added file Q1.py

---
+++ CHAPTER-5/Q1.py
@@ -0,0 +1,11 @@
+# Define a dictionary with words and their English translations
+words = {
+    "bchao": "Help",
+    "kaise ho": "How are you "
+}
+
+# Take input from the user
+word = input("Enter the word ")
+
+# Print the translation of the entered word
+print((words[word]))
