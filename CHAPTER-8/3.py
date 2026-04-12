7fad65dac08fc68152b42124a8fb08fdaf6c62f6
shubham-nov06 <dograshubham06@gmail.com>
2026-04-12 16:03:40 +0000

    🔧 chore(submodule): update CHAPTER-7 submodule pointer

--- CHAPTER-8/3.py
+++ CHAPTER-8/3.py
@@ -35,18 +35,18 @@ print(add(3, 5))
 
 
 def check(num):
-    if num % 2 == 0 :
+    if num % 2 == 0:
         print("Even")
-    else :
+    else:
         print("Odd")
 
 
 def factorial(n):
-    fact = 1 
-    for i in range(1, n+1):
-        fact *=i 
-        
-    return fact 
-    
-    
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+
+    return fact
+
+
 print(factorial(5))
